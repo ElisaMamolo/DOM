9b490f85f2c342cacdc82cb8466789bb201d6144
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-01-11 11:25:00 +0100

    add dom manipulation to index.js

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ The Document Object Model (DOM) is an API (Application Programming Interface)
 */
 
 //document is a structured group of node and objects with properties and methods
+
+
+//search element by 
+//Remember that id must be a string, and it will return the first occurrence only.
+let element = document.getElementById('some-id-goes-here');
+
+//change content of an element
+// index.js
+
+console.log('JS connected'); // <== just a quick check up to make sure js doc is connected properly
+
+// TO GET THE ELEMENT FROM DOM YOU CAN USE "getElementById" 
+let theCatDiv = document.getElementById('cat');
+
+console.log(theCatDiv); // <== what can you see in browser's console
+
+// TO ADD TEXT TO DOM USE "innerHTML" 
+theCatDiv.innerHTML = "I'm a cat";
+
+// set the HTML content of "otherElement" to "I'm a cat"
+otherElement.innerHTML = theCatDiv.innerHTML;
+
