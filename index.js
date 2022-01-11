9f7e280897c00846ad45a59d475010a1dd9a3170
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-01-11 11:06:10 +0100

    first commit -what is the dom

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+Whats is the dom
+
+The Document Object Model (DOM) is an API (Application Programming Interface)
+ for HTML and XML documents. It provides a structured representation of the 
+ document (web page) and defines a way that the structure can be accessed 
+ from JavaScript. This allows us to change the document structure, style or 
+ the content from JavaScript!
+*/
+
+//document is a structured group of node and objects with properties and methods
